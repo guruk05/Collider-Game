e0fdb50d10481ee48cc3abfd20064d6f7c9fda90
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-09-19 20:56:39 +0530

    updated js

--- app.js
+++ app.js
@@ -54,10 +54,7 @@ let body = document.querySelectorAll('body');
 
     let canvas2 = document.createElement('canvas');
 
-    canvas2.setAttribute("id", "canvas2");
-    // canvas2.setAttribute("width", width);
-    // canvas2.setAttribute("height", height);
-
+ 
     body[0].appendChild(canvas2);
 
     let game = canvas2.getContext("2d");
